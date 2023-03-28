c908aa3d15bbf3b2625082d55e3c016eb3129403
Ivan Arellano <captainsolo23@gmail.com>
2023-03-27 18:18:54 -0700

    About ready

---
+++ showSearch/src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='container d-flex flex-column justify-content-center'>
+      <h1>About ShowSearch</h1>
+      <p>ShowSearch is a small web application with informative purposes only</p>
+    </div>
+  )
+}
+
+export default About
--- showSearch/src/routes/Index.jsx
+++ showSearch/src/routes/Index.jsx
@@ -1,12 +1,15 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from '../pages/Home'
 import ShowDetail from '../pages/ShowDetail'
+import About from '../pages/About'
 
 const RoutesIndex = () => {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/shows/:id' element={<ShowDetail />} />
+      <Route path='/about' element={<About />} />
+
     </Routes>
   )
 }
